30283c9ae4052a0e7a637f2efb4f2cc4994e6137
Vladimir Tamara <vtamara@pasosdeJesus.org>
2018-08-08 09:42:03 -0500

    duracion

---
+++ app/models/cor1440_gen/proyectofinanciero.rb
@@ -0,0 +1,19 @@
+# encoding: UTF-8
+
+require 'cor1440_gen/concerns/models/proyectofinanciero'
+
+module Cor1440Gen
+  class Proyectofinanciero < ActiveRecord::Base
+
+    include Cor1440Gen::Concerns::Models::Proyectofinanciero
+    include ApplicationHelper
+
+    @current_usuario = nil
+    attr_accessor :current_usuario
+
+    flotante_localizado :monto
+    flotante_localizado :valorhora
+
+  end
+end
+

c629567639031b605093d3fb19a0fd1146d580ef
Vladimir Tamara <vtamara@pasosdeJesus.org>
2020-01-02 07:54:48 -0500

    Quita tinycolor2 y pick-a-color

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,6 +27,4 @@ import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'
 import 'jquery-ui'
-import 'pick-a-color'
-import tinycolor from 'tinycolor2'
 

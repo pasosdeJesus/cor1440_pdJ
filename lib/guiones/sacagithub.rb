116888b6074869becae997f7c741dc2a64230dea
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-05-05 14:45:54 -0500

    herramienta para extraer issues de github como actividades

--- lib/guiones/sacagithub.rb
+++ lib/guiones/sacagithub.rb
@@ -6,21 +6,30 @@ h = Net::HTTP.get(uri)
 j=JSON.parse(h)
 u = Usuario.where(nusuario: 'vtamara').take || Usuario.take
 j.each do |i|
-  if Cor1440Gen::Actividad.where(urlcaso: i['html_url']).count == 0
+  if Cor1440Gen::Actividad.where(urlcaso: i['html_url']).count == 0 &&
+      i['html_url'].include?('/issues/')
     puts i['title']
-    t = i['title'][/\[[0-9.,]+\]/].to_s[1..-2].to_s.sub(',','.')
-    r = {
-      fecha: Date.today,
-      nombre: i['title'][0..499],
-      oficina_id: 1,
-      usuario_id: u.id,
-      urlcaso: i['html_url'],
-      tiempo: t.to_f
-    }
-    if ENV['fecharep'] then
-      r[:fecharep] = ENV['fecharep']
+    puts i
+    puts "Saltar (s)?"
+    ps = gets.chomp
+    puts "ps=#{ps}"
+    if ps.to_s.strip.length == 0 || ps.to_s.strip.downcase[0] != 's'
+      t = i['title'][/\[[0-9.,]+\]/].to_s[1..-2].to_s.sub(',','.')
+      r = {
+        fecha: Date.today,
+        nombre: i['title'][0..499],
+        oficina_id: 1,
+        usuario_id: u.id,
+        urlcaso: i['html_url'],
+        duracion: t.to_f,
+        medduracion: 'H'
+      }
+      if ENV['fecharep'] then
+        r[:fecharep] = ENV['fecharep']
+      end
+      Cor1440Gen::Actividad.create!(r)
+      puts "Registro creado"
     end
-    Cor1440Gen::Actividad.create!(r)
   end
 end
 

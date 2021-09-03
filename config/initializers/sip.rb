72b5316daef6c6a4c3626ce980d33898bc6d1a96
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-03 10:46:50 -0500

    Actualiza infraestructura

--- config/initializers/sip.rb
+++ config/initializers/sip.rb
@@ -1,11 +1,13 @@
 require 'sip/version'
 
 Sip.setup do |config|
-      config.ruta_anexos = "#{Rails.root}/archivos/"
-      config.ruta_volcados = "#{Rails.root}/archivos/bd/"
-      # En heroku los anexos son super-temporales
-      if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
-        config.ruta_anexos = "#{Rails.root}/tmp/"
-      end
-      config.titulo = "Cor1440 pdJ " + Cor1440Gen::VERSION
+  config.ruta_anexos = (ENV['SIP_RUTA_ANEXOS'] || 
+                        "#{Rails.root}/archivos/anexos/")
+  config.ruta_volcados = (ENV['SIP_RUTA_VOLCADOS'] || 
+                          "#{Rails.root}/archivos/bd/")
+  # En heroku los anexos son super-temporales
+  if ENV["HEROKU_POSTGRESQL_MAUVE_URL"]
+    config.ruta_anexos = "#{Rails.root}/tmp/"
+  end
+  config.titulo = "Cor1440 pdJ " + Cor1440Gen::VERSION
 end

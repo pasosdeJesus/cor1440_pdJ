775d886d57dfa4358e328fe7bd0ba8c111c41b95
Vladimir Támara Patiño <vtamara@pasosdeJesus.org>
2021-09-29 23:20:58 -0500

    permite copiar actividad de listado de actividad con boton en cada fila

--- app/packs/entrypoints/application.js
+++ app/packs/entrypoints/application.js
@@ -1,20 +1,3 @@
-/* eslint no-console:0 */
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-//
-// To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
-// layout file, like app/views/layouts/application.html.erb
-
-
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
-
 console.log('Hola Mundo desde Webpacker')
 
 import Rails from "@rails/ujs"
@@ -28,7 +11,7 @@ window.ApexCharts = ApexCharts
 
 import $ from "expose-loader?exposes=$,jQuery!jquery"
 import 'popper.js'              // Dialogos emergentes usados por bootstrap
-import * as bootstrap from 'bootstrap'              // Maquetacion y elementos de diseño
+import * as bootstrap from 'bootstrap'  // Maquetacion y elementos de diseño
 import 'chosen-js/chosen.jquery';       // Cuadros de seleccion potenciados
 import 'bootstrap-datepicker'
 import 'bootstrap-datepicker/dist/locales/bootstrap-datepicker.es.min.js'

c74fab2c4b4d5ef2bd186870b2643d4d5bba7c51
Nombre Usuario <correo@usuario.net>
2023-03-22 14:49:35 -0500

    mejoras produccion

--- lib/guiones/sacagitlab.rb
+++ lib/guiones/sacagitlab.rb
@@ -2,8 +2,22 @@
 
 require 'net/http'
 
+proyecto_id = ARGV[0].to_i
+
+if proyecto_id && Cor1440Gen::Proyectofinanciero.where(id: proyecto_id).count != 1
+  STDERR.puts "No existe proyecto #{proyecto_id}"
+  exit 1
+end
+
+pf = Cor1440Gen::Proyectofinanciero.find(proyecto_id)
+if pf.idrep_gitlab.to_i == 0
+  STDERR.puts "Proyecto #{pf.nombre} no tiene ID de repositorio en gitlab"
+  exit 1
+end
+
+
 #url = 'https://api.github.com/repos/pasosdeJesus/sivel2_sjrcol/issues?state=closed&sort=updated'
-url = 'https://gitlab.com/api/v4/projects/25947661/issues?state=closed&order_by=updated_at'
+url = "https://gitlab.com/api/v4/projects/#{pf.idrep_gitlab}/issues?state=closed&order_by=updated_at"
 uri = URI.parse(url)
 h = Net::HTTP.get(uri)
 j=JSON.parse(h)
@@ -12,7 +26,7 @@ j.each do |i|
   if i["type"] = "ISSUE" && Cor1440Gen::Actividad.where(urlcaso: i['web_url']).count == 0
     puts "Titulo: #{i['title']}"
     puts "Saltar (s)?"
-    ps = gets.chomp
+    ps = 'n' #gets.chomp
     puts "ps=#{ps}"
     if ps.to_s.strip.length == 0 || ps.to_s.strip.downcase[0] != 's'
       t = i['title'][/\[[0-9.,]+\]/].to_s[1..-2].to_s.sub(',','.')
@@ -28,7 +42,13 @@ j.each do |i|
       if ENV['fecharep'] then
         r[:fecharep] = ENV['fecharep']
       end
-      Cor1440Gen::Actividad.create!(r)
+      a = Cor1440Gen::Actividad.create!(r)
+      if proyecto_id > 0
+        Cor1440Gen::ActividadProyectofinanciero.create!(
+          actividad_id: a.id,
+          proyectofinanciero_id: proyecto_id
+        )
+      end
       puts "Registro creado"
     end
   end
